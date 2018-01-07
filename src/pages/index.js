5ffd782185941ebb402fe26373dee5eafd64c823
agonzalez0515 <agonzalez0515@gmail.com>
2018-01-06 23:37:50 -0800

    flexbox works! mostly

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,25 +3,24 @@ import Link from 'gatsby-link'
 import Article from '../components/article'
 
 const IndexPage = ({data}) => {
-  // const {title, link, linkToImage} = data.allContentfulArticle.edges[0].node
   const allArticles = data.allContentfulArticle.edges
   
-
   return (
     <div css={{
       display: `flex`,
       flexFlow: `row wrap`,
-      justifyContent: `space-around`,
+      justifyContent: `space-evenly`,
       alignItems: `center`,
       padding: `0`,
       marginTop: `20px`,
+      height: `500px`
     }}
     >
     {
       allArticles.map(({node:article}) =>
-      <Article title={article.title} link={article.link} linkToImage={article.linkToImage} /> 
+      <Article title={article.title} link={article.link} linkToImage={ article.image == null ? article.linkToImage : article.image.file.url} /> 
     )}
-    </div>
+    </div> 
   )
 }
 
@@ -30,12 +29,15 @@ export default IndexPage
 export const contentQuery = graphql`
   query Content {
     allContentfulArticle {
-      edges{
+      edges {
         node {
           title
           link
-          linkToImage
-          
+          image {
+            file {
+              url
+            }
+          }
         }
       }
     }
