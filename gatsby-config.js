4e7b09b0275dbd92866b0a3a49291fd1a4236ba1
agonzalez0515 <agonzalez0515@gmail.com>
2018-01-10 21:53:46 -0800

    add sass plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-glamor`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
