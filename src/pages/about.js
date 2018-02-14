3d37c5c1d5263b327d00d1e908501432059dd1b4
agonzalez0515 <agonzalez0515@gmail.com>
2018-02-13 18:57:57 -0800

    change to css grid

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,49 +4,19 @@ import profile from '../assets/profile.jpg'
 import '../style.scss'
 
 const About = () => (
-  <div className= "cont" css={{
-    display: `flex`,
-    flexFlow: `row wrap`,
-    justifyContent: `space-around`,
-    alignItems: `center`,
-    marginTop: `50px`
-  }} >
-    <div css={{
-      flexShrink:`0`,
-      '@media (max-width: 415px)': {
-        order: `2`
-      }
-    }}>
-      <h2 className="author"> About </h2>
-      <h2 className="author"> Me </h2>
-      <div className="bio" css={{
-        overflow: `auto`,
-        fontSize: `1.2rem`, 
-        width: `620px`, 
-        margin: `20px`, 
-        padding: `5px`,
-        '@media (max-width: 415px)': {
-          width: `450px`
-          }
-      }}>
-        <p>Writer and filmmaker, Natalia Provatas, is a first generation Los Angeles native of Puerto Rican and Greek descent. </p>
-        <p>Last year, her personal essay <a href="http://www.refinery29.com/2017/02/139706/bulletproof-biohacking-conference-2016">"What It’s Really Like Inside the Elite Conference for Health Bros”</a> for Refinery 29 was voted one of Apple News’ “Weekly Must Reads.” Additional published work can be found on <a href="https://www.damemagazine.com/2014/09/22/my-vagina-and-me-love-story/">Dame Magazine</a>, <a href="https://www.wework.com/creator/personal-profiles/on-being-a-tomboy/?utm_source=wework&utm_medium=referral&utm_campaign=creator">Creator Magazine</a>, <a href="http://www.thegloss.com/author/natalia-provatas/">The Gloss</a>, and <a href="http://manifestjustice.org/blog/post/2">Manifest Justice</a>.  She was also project manager for <a href="http://ontheinsideart.com/">On the Inside</a>, the largest art collection of currently incarcerated LGBTQ prisoners that opened at Abron's Art Center in NYC last fall, and will be coming to The Craft and Folk Art Museum in Los Angeles summer of 2019.</p>
-        <p>Natalia co-directed/co-wrote the dark comedy short film Canary Suicides, which won the "Audience Award" at Holly Shorts, and was an official selection at both SXSW and Cleveland International Film Festival. She also directed and co-wrote the comedy web series Lazy Actress. Previously, Natalia was a production and editorial coordinator for the DreamWorks Animation features, The Croods, Me and My Shadow and Monkeys of Mumbai, respectively. She also worked on the writing team of BOO: Earth Division for DreamWorks Animation Television.</p>
-        <p>Growing up, Natalia was drawn to writing as a way to share her struggles about trying to fit into the world at large as a mixed-race American. She also writes about sex, the body, female identity, and surviving breast cancer from a multi-culture lens.</p>  
-        <p>She currently resides in Silverlake with her fluffy dog, Norman. Both dance for treats.</p>
-      </div>
-    </div>
-    <div css={{
-      flexShrink:`0`,
-      padding: `5px`,
-      width: `30%`,
-      '@media (max-width: 415px)': {
-        order: `1`
-      }
-  }}>
-    <img src={profile} alt="picture of Natalia Provatas" css={{maxWidth: `100%`}} />
-  </div>
-  
+  <div className= "container" css={{display: `grid`, gridColumnGap:`20px`, gridTemplateColumns: `400px`}}>
+    <h2 className="author" css={{gridColumn: `span 4`, justifySelf: `start`}}> About </h2>
+    <h2 className="author" css={{gridColumn: `span 4`, justifySelf: `start`}}> Me </h2>
+    <section className="bio" css={{gridColumn: `span 2`}}>
+      <p>Writer and filmmaker, Natalia Provatas, is a first generation Los Angeles native of Puerto Rican and Greek descent. </p>
+      <p>Last year, her personal essay <a href="http://www.refinery29.com/2017/02/139706/bulletproof-biohacking-conference-2016">"What It’s Really Like Inside the Elite Conference for Health Bros”</a> for Refinery 29 was voted one of Apple News’ “Weekly Must Reads.” Additional published work can be found on <a href="https://www.damemagazine.com/2014/09/22/my-vagina-and-me-love-story/">Dame Magazine</a>, <a href="https://www.wework.com/creator/personal-profiles/on-being-a-tomboy/?utm_source=wework&utm_medium=referral&utm_campaign=creator">Creator Magazine</a>, <a href="http://www.thegloss.com/author/natalia-provatas/">The Gloss</a>, and <a href="http://manifestjustice.org/blog/post/2">Manifest Justice</a>.  She was also project manager for <a href="http://ontheinsideart.com/">On the Inside</a>, the largest art collection of currently incarcerated LGBTQ prisoners that opened at Abron's Art Center in NYC last fall, and will be coming to The Craft and Folk Art Museum in Los Angeles summer of 2019.</p>
+      <p>Natalia co-directed/co-wrote the dark comedy short film Canary Suicides, which won the "Audience Award" at Holly Shorts, and was an official selection at both SXSW and Cleveland International Film Festival. She also directed and co-wrote the comedy web series Lazy Actress. Previously, Natalia was a production and editorial coordinator for the DreamWorks Animation features, The Croods, Me and My Shadow and Monkeys of Mumbai, respectively. She also worked on the writing team of BOO: Earth Division for DreamWorks Animation Television.</p>
+      <p>Growing up, Natalia was drawn to writing as a way to share her struggles about trying to fit into the world at large as a mixed-race American. She also writes about sex, the body, female identity, and surviving breast cancer from a multi-culture lens.</p>  
+      <p>She currently resides in Silverlake with her fluffy dog, Norman. Both dance for treats.</p>
+    </section>
+    <aside css={{alignSelf: `center`}}>
+      <img src={profile} alt="picture of Natalia Provatas" css={{width: `350px`, display: `block`}} />
+    </aside>
   </div>
 )
 
