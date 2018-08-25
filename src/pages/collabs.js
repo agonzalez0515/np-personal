9634146455ab3cc916f68172fecc1de6579f196c
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-08-24 20:47:11 -0700

    Update collabs link and about page flex. Add blue background

--- src/pages/collabs.js
+++ src/pages/collabs.js
@@ -2,8 +2,8 @@ import React from 'react'
 import '../style.scss'
 
 const Collabs = () => (
-  <main> 
-    <h2 css={{fontSize: `5rem`, textAlign: `center`}}><a href="http://ontheinsideart.com/" css={{color: `black`}}> On The Inside </a></h2>
+  <main className="collabs"> 
+    <h2 data-heading="On The Inside"><a href="http://ontheinsideart.com/" css={{color: `black`}}> On The Inside </a></h2>
     <p css={{textAlign: `center`}}>
     Project Manager for On The Inside, largest private collection of currently incarcerated LGBTQ artists.
     </p>
