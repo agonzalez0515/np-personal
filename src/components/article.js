807bf75e1c9b7b2da2fbd2e1f2bfe67cae177250
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-10-27 17:55:20 -0700

    refactor css out to stylesheet

--- src/components/article.js
+++ src/components/article.js
@@ -5,31 +5,16 @@ const Article = props =>
   <div className="article-item">
     <a href={props.link}> 
       <figure css={{marginBottom: `.1rem`}}>
-        <img src={props.linkToImage}
-             alt={props.description}
-            css={{
-                width: `100%`,
-                margin: `0`,
-                boxShadow: `0 10px 30px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)`
-            }} />
+        <img src={props.linkToImage} alt={props.description} />
             <figcaption> {props.caption} </figcaption>     
       </figure>
     </a>
-   
-    <div css={{margin:`auto`}}>
-      <p className="article-title"
-          css={{
-              textAlign: `center`,
-              width: `90%`,
-              paddingTop: `2%`,
-              marginLeft: `auto`,
-              marginRight: `auto`                   
-          }}>
-          <a css={{color: `black`, fontSize: `1.1rem`}} href={props.link}> {props.title} </a>
+    <div className="article-title-container">
+      <p className="article-title">
+        <a href={props.link}> {props.title} </a>
       </p>
     </div>
   </div>
        
-
 export default Article
 
