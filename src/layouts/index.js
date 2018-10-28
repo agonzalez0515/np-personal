dcf937ed9b64bc06b7815e794367ea7b9c0e7c01
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-10-27 17:59:20 -0700

    Refactor NavLink component out and css into stylesheet

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,47 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import Header from '../components/header'
+import NavList from '../components/nav'
 import '../style.scss'
 
-
-    const NavList= props =>
-      <li className="nav-item">
-        <Link 
-          css={{
-            
-          }}
-          to={props.to}>
-            {props.children}
-        </Link>
-      </li>
-  
 
 //the div below the header is the container for the body
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet>
+      <meta charset="UTF-8" /> 
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="Natalia Provatas: Writer and Filmmaker" /> 
+      <meta name="keywords" content="writer, filmmaker, content creator, refinary 29, the gloss" />
+      {/* <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon"></link> */}
       <title>Natalia Provatas</title>
     </Helmet>
     <Header />
     <nav>
       <ul role="navigation" className="nav">
         <NavList to="/">published works</NavList>
-        <NavList to="/films">films</NavList>
         <NavList to="/collabs">collabs</NavList>
+        <NavList to="/films">original content</NavList>
         <NavList to="/about">about</NavList>
       </ul>
     </nav>
-    <div
-      css={{
-        margin: '0 auto',
-        width: `auto`,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="content">
       {children()}
     </div>
   </div>
