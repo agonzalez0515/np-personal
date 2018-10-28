a2e581c942661815826d289a1fd63f065f9869e4
Angelica Gonzalez <agonzalez0515@gmail.com>
2018-10-27 17:14:30 -0700

    refactor css out to stylesheet

--- src/components/header.js
+++ src/components/header.js
@@ -1,68 +1,31 @@
 import Link from 'gatsby-link'
 
 
-const Header = props =>
+const Header = () =>
+  <div>
     <div>
-    <div>
-      <ul css={{
-        overflow: `hidden`,
-       margin: 0,
-       paddingRight: `2%`,
-       fontSize: `1.1em`
-      }}>
-        <li className="media-icons">
+      <ul className="socials">
+        <li className="social-links">
             <a href="https://twitter.com/nataliaprovatas">twitter</a>
         </li>
-        <li className="media-icons">
+        <li className="social-links">
             <a href="https://www.instagram.com/chola4hire/">instagram</a>
         </li>
-        <li className="media-icons">
+        <li className="social-links">
             <a href="mailto:nataliaprovatas@gmail.com" target="_top">email</a>
         </li>
       </ul>
     </div>
-    <div
-      css={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 css={{ 
-        margin: 0, 
-        fontSize: `5rem`,
-        textAlign: `center`,
-        paddingTop: `2rem` 
-      }}
-      >
-        <Link
-          to="/"
-          css={{
-            color: `#ff5e62`,
-            ':hover': {
-              textDecoration: `none`,
-            },
-            '@media (max-width: 415px)': {
-              fontSize: `3.8rem`,
-              paddingTop: `10px` 
-            },
-            '@media (max-width: 815px)': {
-              fontSize: `3rem`,
-              paddingTop: `10px` 
-            }
-          }}
-        >
+    <div className="header">
+      <h1>
+        <Link to="/" className="name">
           NATALIA PROVATAS
         </Link>
       </h1>
-      <p css={{
-        fontSize: `1.4rem`,
-        textAlign: `center`,
-        letterSpacing: `3px`,
-        paddingTop: `3%` 
-      }}>
+      <p className="sub-heading">
           WRITER | FILMMAKER
       </p>
     </div>
   </div>
+
 export default Header
